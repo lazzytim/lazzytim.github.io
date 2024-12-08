0d71b0b002c60120e06749e07651538739f00396
LAZZYTIM <deletrez.timothy@gmail.com>
2024-12-08 11:27:03 +0100

    🔁 Updated index/html, style.css, app.js
    - Added arrowlink.svg
    - Added github.png
    - Added mail.png
    - Edited index.html
    - Edited style.css
    - Edited app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// Mobile Menu Opening
+// // Mobile Menu Opening
 
-const menu = document.querySelector('.sidepanel')
-const open = document.querySelector('.burger')
-const close = document.querySelector('.cross')
+// const menu = document.querySelector('.sidepanel')
+// const open = document.querySelector('.burger')
+// const close = document.querySelector('.cross')
 
-open.addEventListener('click', () => {
-    menu.classList.add('active')
-})
+// open.addEventListener('click', () => {
+//     menu.classList.add('active')
+// })
 
-close.addEventListener('click', () => {
-    menu.classList.remove('active')
-})
+// close.addEventListener('click', () => {
+//     menu.classList.remove('active')
+// })
 
 // Scroll Animations
 
@@ -18,7 +18,7 @@ const obeserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.add('AnimShow')
+            entry.target.classList.add('appAnimatedShow')
         } 
         
         // else {
@@ -27,5 +27,5 @@ const obeserver = new IntersectionObserver((entries) => {
     })
 })
 
-const hiddenElements = document.querySelectorAll('.AnimHidden')
+const hiddenElements = document.querySelectorAll('.appAnimated')
 hiddenElements.forEach((el) => obeserver.observe(el))
